d38be8f9fd3f3cf5e1d2cdfc1576aa2f83a3fcab
EmersonHoruss <peralesvillanuevaemerson@gmail.com>
2023-12-20 13:01:11 -0500

    done

--- 01_building_blocks/Zoo.java
+++ 01_building_blocks/Zoo.java
@@ -1,5 +1,11 @@
+/*
+run in console:
+javac Zoo.java
+java Zoo Bronx Zoo
+*/
 public class Zoo {
 	public static void main(String[] args){
-		System.out.println("Hello World");
+		System.out.println(args[0]);
+		System.out.println(args[1]);
 	}
 }
