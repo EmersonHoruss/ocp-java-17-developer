b7e794b3d1482515ee6fb2cd72464effe35e770e
EmersonHoruss <peralesvillanuevaemerson@gmail.com>
2023-12-21 11:28:11 -0500

    redundant imports

--- 01_building_blocks/NumberPicker.java
+++ 01_building_blocks/NumberPicker.java
@@ -1,4 +1,7 @@
-import java.util.*;
+import java.lang.System; // java.lang is an special package that it is automatically imported
+import java.lang.*; // it is redudant as the first for the same reason and because is already imported in System
+import java.util.Random; 
+import java.util.*; // it is redundant because is already imported Rnadom
 public class NumberPicker {
 	public static void main(String[] args){
 		Random r = new Random();
